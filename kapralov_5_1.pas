1d8e9e588b6e4fd9bdf30ed487352a1e00478bb4
Alex <31609676+ASdzxcqweasdzxc@users.noreply.github.com>
2021-10-17 20:43:10 +0300

    Add files via upload

--- kapralov_5_1.pas
+++ kapralov_5_1.pas
@@ -10,6 +10,13 @@ begin
   writeln('enter 1st number: '); readln(x);
   writeln('enter 2nd number: '); readln(y);
 
+  if (c = '/') and (y = 0) then
+  begin
+  write('Division by 0 not defined!');
+  readln;
+  exit;
+  end;
+
   case c of
     '+': writeln((x+y):5:2);
     '-': writeln((x-y):5:2);
