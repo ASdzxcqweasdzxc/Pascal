934ca4cee2467b316c7316b5c432340db8c5d62f
Alex <31609676+ASdzxcqweasdzxc@users.noreply.github.com>
2021-12-21 00:12:23 +0300

    Add files via upload

--- kapralov_17/kapralov_17.pas
+++ kapralov_17/kapralov_17.pas
@@ -4,7 +4,7 @@
  * Date: 06.12.2021
  *)
 program kapralov_17;
-uses obj, files, sort;
+uses obj, files, sort, natMerge, newMerge, natMrg;
 
 var
   towns: arrOfTowns;
@@ -18,12 +18,13 @@ begin
    * writeln('Second sorting method: ', abs((711 + 212) mod 4) + 6); //= 9
    * writeln('Working data: ', abs((711 + 123) mod 11) + 1); //= 10
    *)
-  n:= 5;
+  n:= 47 - 1;
   seq:= 1;
   name:= '';
   demo:= 0;
   comps:= 0;
   changes:= 0;
+  //towns:= ;
   
   case seq of
     1: name:= ('townsSeq1.txt');
@@ -35,15 +36,19 @@ begin
 
   //procedure rFile(var arr: arrOfTowns; n, demo: integer; fName: string);
   rFile(towns, n, name);
-  print(towns, n);
-  writeln;
+  //print(towns, n);
+  //writeln;
 
-  //procedure bubble_sort(var arr: arrOfTowns; var count: integer; max_n, mode: integer);
-  bubble_sort(towns, comps, changes, n, demo);
+  //procedure bubble_sort(var arr: arrOfTowns; var count: integer; max_n, moder: integer);
+  //bubble_sort(towns, comps, changes, n, demo);
   //procedure fillRes(var arr: arrOfRes; i: integer; var comps: integer; var changes: integer);
   fillRes(result, seq, comps, changes);
   if (demo = 0) then print(towns, n);
   writeln('Results for ', name, ': comparisons ',result[1].comps, ', changes ', result[1].changes, '. Total of ', result[1].comps + result[1].changes, ' iterations.');
-      
+  
+  //Natural_Merging_Sort(var arr: arrOfTowns; var comps: integer; var changes: integer; max_n, mode: integer);
+  Natural_Merging_Sort(towns, comps, changes, n, demo);
+  //Nat_sort(towns, comps, changes, n, demo);
+  //print(towns, n); 
 ReadLn
 end.  
